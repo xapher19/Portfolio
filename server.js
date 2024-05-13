6c1582fd7064d11775bfd09878dd5c7771c5d7b6
Ashley Sweeney <git@ashleysweeney.dev>
2024-05-13 20:26:40 +0100

    Update 1

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/about.html'));
+});
+
 app.use('/public', express.static('public'));
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(8080, () => console.log('Example app is listening on port 8080.'));
