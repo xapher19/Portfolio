253aa65eb68c0d7caf82fe5e3157b8f8c2afb612
Ashley Sweeney <git@ashleysweeney.dev>
2024-05-14 19:26:46 +0100

    Main layout finished

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+// Set consts
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
+// Direct root domain to index html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/about.html'));
-});
+// Use Cache-Control
+/* app.use((req, res, next) => {
+    res.set('Cache-Control', 'public, max-age=31557600');
+    next();
+});*/
 
+// Use public folder for static files
 app.use('/public', express.static('public'));
 
+// Open port
 app.listen(8080, () => console.log('Example app is listening on port 8080.'));
